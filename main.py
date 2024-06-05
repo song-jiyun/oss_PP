8395d3cae7f8ceed80936eff2f153a54b50feeb7
song-jiyun <jysong2001@g.skku.edu>
2024-06-05 19:57:53 +0900

    백엔드: uncover()

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import random
 fps = 60                #게임의 fps
 screen_width = 1920     #창 너비
 screen_height = 1080    #창 높이
-field_width = 9        #게임판 너비
-field_height = 9       #게임판 높이
-mines = 10              #지뢰 갯수
+field_width = 30        #게임판 너비
+field_height = 16       #게임판 높이
+mines = 99              #지뢰 갯수
 #기본 상수 정의
 
 #색상 정의
@@ -28,43 +28,43 @@ field_cover = []    #가림막 정보, 0은 공개된 위치, 1은 가려진 위
 #게임판
 
 #해당 칸의 지뢰 여부 판별
-def ismine(x, y):
+def isMine(x, y):
     return (field[x][y] == 9)
 #해당 칸의 지뢰 여부 판별
 
 #주변의 지뢰 갯수 세기
-def countmine(x, y):
+def countMine(x, y):
     count = 0
     if(x != 0):
-        if(ismine(x - 1, y)):
+        if(isMine(x - 1, y)):
             count += 1
 
         if(y != 0):
-            if(ismine(x - 1, y - 1)):
+            if(isMine(x - 1, y - 1)):
                 count += 1
         
         if(y != field_height - 1):
-            if(ismine(x - 1, y + 1)):
+            if(isMine(x - 1, y + 1)):
                 count += 1
 
     if(x != field_width - 1):
-        if(ismine(x + 1, y)):
+        if(isMine(x + 1, y)):
             count += 1
 
         if(y != 0):
-            if(ismine(x + 1, y - 1)):
+            if(isMine(x + 1, y - 1)):
                 count += 1
 
         if(y != field_height - 1):
-            if(ismine(x + 1, y + 1)):
+            if(isMine(x + 1, y + 1)):
                 count += 1
 
     if(y != 0):
-        if(ismine(x, y - 1)):
+        if(isMine(x, y - 1)):
             count += 1
 
     if(y != field_height - 1):
-        if(ismine(x, y + 1)):
+        if(isMine(x, y + 1)):
             count += 1
 
     return count
@@ -86,8 +86,8 @@ def gameSetup():
     i = 0
     while(i < mines):
         rand = random.randrange(0, field_width * field_height)
-        x = rand // field_width
-        y = rand % field_width
+        x = rand // field_height
+        y = rand % field_height
         if(field[x][y] == 9):
             continue
         i += 1
@@ -98,10 +98,61 @@ def gameSetup():
     for x in range(0, field_width):
         for y in range(0, field_height):
             if(field[x][y] != 9):
-                field[x][y] = countmine(x, y)
+                field[x][y] = countMine(x, y)
     #지뢰가 없는 칸에 주변 지뢰 갯수 입력
-
 #게임판 만들기
+
+#게임종료
+def gameover(success):
+    if(success == False):
+        for x in range(0, field_width):
+            for y in range(0, field_height):
+                if(isMine(x, y)):
+                    field_cover[x][y] = 0
+    else:
+        return
+        
+    
+#게임종료
+
+#칸 열기
+def uncover(x, y):
+    
+    #해당칸이 0이면 주변칸도 열어야 함
+    if(field[x][y] == 0):
+        if(x != 0):
+            uncover(x - 1, y)
+
+            if(y != 0):
+                uncover(x - 1, y - 1)
+        
+            if(y != field_height - 1):
+                uncover(x - 1, y + 1)
+
+        if(x != field_width - 1):
+            uncover(x + 1, y)
+
+            if(y != 0):
+                uncover(x + 1, y - 1)
+
+            if(y != field_height - 1):
+                uncover(x + 1, y + 1)
+
+        if(y != 0):
+            uncover(x, y - 1)
+
+        if(y != field_height - 1):
+            uncover(x, y + 1)
+    #해당칸이 0이면 주변칸도 열어야 함
+    
+    #해당칸이 지뢰면 게임오버
+    elif(isMine(x, y)):
+        gameover(False)
+    #해당칸이 지뢰면 게임오버
+
+    else:
+        field_cover[x][y] = 0
+#칸 열기
 
     
 #게임 시작
